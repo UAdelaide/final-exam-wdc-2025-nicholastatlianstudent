35ad2221f2a28638dc12567acdf5c28542f7eb14
nicholastatlianstudent <nicholas.tatlian@student.adelaide.edu.au>
2025-06-20 11:40:39 +0000

    [automatic commit] 2025-06-20 11:40:38 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -147,20 +147,8 @@ app.get('/api/walkrequests/open', (req, res) => {
     });
 });
 
-app.get('/api/walkrequests/open', (req, res) => {
-    db.query(`
-        SELECT
-            WalkRequests.request_id,
-            Dogs.name AS dog_name,
-            WalkRequests.requested_time,
-            WalkRequests.duration_minutes,
-            WalkRequests.location,
-            Users.username AS owner_username
-        FROM WalkRequests
-        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-        JOIN Users ON Dogs.owner_id = Users.user_id
-        WHERE WalkRequests.status = 'open'
-    `, (err, results) => {
+app.get('/api/walkrequests/summary', (req, res) => {
+    db.query(, (err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Failed to fetch open walk requests' });
