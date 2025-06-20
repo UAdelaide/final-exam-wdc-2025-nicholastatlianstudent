8274a0335061dd57cbe90668b85c64a12e23feed
nicholastatlianstudent <nicholas.tatlian@student.adelaide.edu.au>
2025-06-20 11:33:32 +0000

    [automatic commit] 2025-06-20 11:33:32 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -126,7 +126,16 @@ app.get('/api/dogs', async (req, res) => {
 });
 
 app.get('api/walkrequests/open', async (req, res) => {
-  db.query('SELECT WalkRequests.request_id, ')
+  db.query('WalkRequests.request_id,
+      Dogs.name AS dog_name,
+      WalkRequests.requested_time,
+      WalkRequests.duration_minutes,
+      WalkRequests.location,
+      Users.username AS owner_username
+    FROM WalkRequests
+    JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+    JOIN Users ON Dogs.owner_id = Users.user_id
+    WHERE WalkRequests.status = 'open')
 })
 
 
